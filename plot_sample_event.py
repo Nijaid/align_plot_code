d4f934fc67eac7e54b4286876ffe8612154234c8
Nijaid <22627046+Nijaid@users.noreply.github.com>
2019-07-08 16:42:31 -0700

    Made plot_sample_event more adaptive, updated gitignore

--- plot_sample_event.py
+++ plot_sample_event.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 t = np.logspace(-2, 2, 10000)
 pi_rel = 4.848e-9*(1./4.0 - 1./8.0)*3600.*1000.*180./np.pi
@@ -14,11 +15,15 @@ ax = plt.axes()
 ax.set_xscale('log')
 ax.grid(True, which='both', linestyle=':', color='grey')
 
-plt.plot(t, shift, 'k', label='centroid shift x10 [mas]')
 plt.plot(t, amp, 'k--', label='amplification')
+plt.plot(t, shift, 'k', label='centroid shift x10 (mas)')
 plt.xlabel(r'$\left( t - t_0 \right)\; / \; t_E$')
 plt.legend()
 plt.xlim(t[0], t[-1])
-plt.savefig('shift_mag_5.0.pdf', bbox_inches='tight')
+if os.path.exists('../paper_plots/'):
+    plt.savefig('../paper_plots/shift_mag_5.pdf', bbox_inches='tight')
+else:
+    out_dir = raw_input("Where would you like to save? ")
+    plt.savefig(out_dir + '/shift_mag_5.pdf', bbox_inches='tight')
 
 plt.close(fig)
